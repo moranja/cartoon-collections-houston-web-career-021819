95e3086681024f3a5c6f6f1fcc1be5d5a25384b3
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 00:53:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteers)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|i| i.length >= 5}
 end
 
 def find_the_cheese# code an argument here
