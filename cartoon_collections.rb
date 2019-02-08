b714a2f7979d4f6f4113f9feda32d9cf257e9d5f
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 00:55:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ def long_planeteer_calls(words)
   words.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
