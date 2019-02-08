effaf6de937ed4cbafe1e4279fdaca1aeeeb773d
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 00:46:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet
+def summon_captain_planet(planeteers)
+  planeteers.collect do |element|
+    
 end
 
 def long_planeteer_calls# code an argument here
