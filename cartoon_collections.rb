c4bd5330c4c2be544cd3df57be28140262f696d8
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 00:47:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteers)
-  planeteers.collect do |element|
+  planeteers.map do |element|
     element.capitalize << "!"
   end
 end
