e4b8d1c8390ca6d32b3e2ac404e4cad65ee22fe6
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 00:57:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if food.include?(cheese_types[0]) == TRUE {puts }
+  if food.include?(cheese_types[0]) == TRUE {return food.first(cheese_types[0])}
 end
